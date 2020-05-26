66d3b1a21de62271ff0354ae43833a312b802a6e
Alex <cond0068@flinders.edu.au>
2020-05-26 15:56:36 +0930

    foundations of GUI

--- studentDBGUI/src/studentDB.java
+++ studentDBGUI/src/studentDB.java
@@ -1,2 +1,70 @@
-public class studentDB {
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.Event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class studentDB extends JFrame {
+    private JPanel mainPanel;
+    private JLabel headerLabel;
+    private JPanel studentDetailsPanel;
+    private JPanel topicDetailsPanel;
+    private JPanel spacePanel;
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JButton addTopicResultButton;
+    private JButton findTopicResultButton;
+    private JComboBox comboBox2;
+    private JButton addStudentButton;
+    private JButton findStudentButton;
+    private JButton printAllRecordsButton;
+    private JButton clearAllRecordsButton;
+    private JPanel degreeOptionsPane;
+    private JPanel awardPrizesPane;
+    private JTextPane textPane1;
+
+    public studentDB (String title) {
+        super(title);
+
+        mainPanel.setPreferredSize(new Dimension(800,450));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+
+
+
+
+    }
+
+    public static void main (String[] args) {
+
+        JFrame frame = new studentDB("Student Database Program");
+        frame.setVisible(true);
+
+        // Menu Bar
+        JMenuBar menuBar = new JMenuBar();
+        frame.setJMenuBar(menuBar);
+
+        JMenu file = new JMenu("File");
+        menuBar.add(file);
+
+        JMenuItem exit = new JMenuItem("Exit");
+        file.add(exit);
+
+        class exitaction implements ActionListener {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        }
+        exit.addActionListener(new exitaction());
+
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
+
+
